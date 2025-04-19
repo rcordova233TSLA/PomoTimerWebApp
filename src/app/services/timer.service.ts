f7597f3f36bae822c72b2ca5e3f14c5bc638de8d
Robert Cordova <77909709+rcordova233TSLA@users.noreply.github.com>
2025-04-19 18:36:34 -0400

    untested changes when ending/restarting timer but start looks good

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -1,16 +1,51 @@
 import { Injectable } from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { interval, Observable,take } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TimerService {
-    timeObservable:Observable<number>
-    constructor() { 
-        this.timeObservable = interval(1000);
+    startTime: number|null = null 
+    configuredDuration!: number //seconds
+    activeInterval:Observable<number>|null=null;
+
+    constructor() {}
+    configureTimer(minutes:number,seconds:number)
+    {
+        this.configuredDuration = minutes*60+seconds;
+
+    }
+    createNewInterval(seconds:number):Observable<number>
+	{
+        const newInterval = interval(1000); //emit every second
+        const intervalTake = newInterval.pipe(take(seconds));
+        return intervalTake;
+    }
+    StartTimer():Observable<number>
+    {
+        if (this.startTime == null)
+        {
+            // Fresh start after reset or upon first start
+            this.startTime = Date.now()
+            this.activeInterval = this.createNewInterval(this.configuredDuration);	
+        }
+        else
+        {
+            //Assuming a pause/start has happened
+            const currentTime = Date.now()
+            const timeLeft:number = currentTime - this.startTime
+            this.activeInterval = this.createNewInterval(timeLeft)
+        }
+        return this.activeInterval;
     }
 
-    getTimerObservable():Observable<number>
+    StopTimer()
     {
-        return this.timeObservable;
+        this.activeInterval = null;
+    }
+
+    ResetTimer()
+    {
+        this.StopTimer()
+        this.startTime = null;
     }
 }
--- src/app/time-player/time-player.component.ts
+++ src/app/time-player/time-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { TimerService } from '../services/timer.service';
 import { Observable, Subscription, take, timer } from 'rxjs';
 
@@ -9,18 +9,51 @@ import { Observable, Subscription, take, timer } from 'rxjs';
   styleUrl: './time-player.component.scss'
 })
 export class TimePlayerComponent implements OnDestroy{
-    currentMinutes:number = 25;
-    currentSeconds:number = 0;
-    timerSub = new Subscription();
-    numberToTake:Observable<number>;
+    @Input() scheduledMinutes:number=25
+    @Input() timerSeconds:number = 0
+    minutesLeft:number;
+    secondsLeft:number;
+    currentSub!: Subscription
     constructor(private timerService:TimerService){
-        this.numberToTake = timerService.getTimerObservable().pipe(take(25));
-        this.timerSub = this.numberToTake.subscribe((x)=>{
-            console.log('Next val: ',x);
-        });
+        timerService.configureTimer(this.scheduledMinutes,this.timerSeconds)
+        this.minutesLeft = this.scheduledMinutes
+        this.secondsLeft = this.timerSeconds
     }
 
     ngOnDestroy(): void {
-        this.timerSub.unsubscribe();
+        if (this.currentSub!=undefined || this.currentSub!=null)
+        {
+            this.currentSub.unsubscribe();
+        }
+    }
+
+    onStartButton()
+	{
+        const intervalObservable = this.timerService.StartTimer()
+        this.currentSub = intervalObservable.subscribe(
+            (secondsPassed)=>{
+                console.log(secondsPassed);
+                this.updateTimeFields()
+            }
+        )
+    }
+    updateTimeFields()
+    {
+        const updatedTime = (this.minutesLeft*60+this.secondsLeft) -1;
+        this.minutesLeft = Math.floor(updatedTime/60)
+        this.secondsLeft = updatedTime%60
+    }
+    onPauseButton()
+    { 
+        this.currentSub.unsubscribe()
+        this.timerService.StopTimer()
+    }
+
+    onResetButton()
+    {
+        this.currentSub.unsubscribe()
+        this.timerService.ResetTimer()
+        this.minutesLeft = this.scheduledMinutes // change to scheduledMinutes
+        this.secondsLeft = this.timerSeconds // change to scheduledSeconds
     }
 }
