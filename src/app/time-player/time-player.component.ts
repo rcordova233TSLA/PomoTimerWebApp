0bb63f8d100f36580e49c742cee0c4bbd8aaad63
Robert Cordova <77909709+rcordova233TSLA@users.noreply.github.com>
2025-04-19 21:23:47 -0400

    disabling/enabling of start/pause buttons

--- src/app/time-player/time-player.component.ts
+++ src/app/time-player/time-player.component.ts
@@ -2,7 +2,12 @@ import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/
 import { TimerService } from '../services/timer.service';
 import { Observable, Subscription, take, timer } from 'rxjs';
 import { TimerConfig } from './TimerConfiguration';
-
+enum TimerState
+{
+    OFF,
+    PAUSED,
+    RUNNING
+}
 @Component({
   selector: 'app-time-player',
   imports: [],
@@ -10,17 +15,15 @@ import { TimerConfig } from './TimerConfiguration';
   styleUrl: './time-player.component.scss'
 })
 export class TimePlayerComponent implements OnDestroy,OnChanges{
-    // scheduledMinutes:number;
-    // scheduledSeconds:number;
+    state:TimerState;
     @Input() configuration:TimerConfig = {minutes:25,seconds:0};
     minutesLeft!:number;
     secondsLeft!:number;
     currentSub!: Subscription
     constructor(private timerService:TimerService){
-        // this.scheduledMinutes = this.configuration.minutes
-        // this.scheduledSeconds = this.configuration.seconds
         timerService.configureTimer(this.configuration.minutes,this.configuration.seconds)
-        this.setTimeBasedOfCfg()
+        this.setTimeBasedOfCfg();
+        this.state = TimerState.OFF;
     }
     setTimeBasedOfCfg()
     {
@@ -47,13 +50,18 @@ export class TimePlayerComponent implements OnDestroy,OnChanges{
 
     onStartButton()
 	{
-        const intervalObservable = this.timerService.StartTimer()
-        this.currentSub = intervalObservable.subscribe(
-            (secondsPassed)=>{
-                console.log(secondsPassed);
-                this.updateTimeFields()
-            }
-        )
+        if (!this.isTimerRunning())
+        {
+            this.state = TimerState.RUNNING;
+            console.log("Starting Timer");
+            const intervalObservable = this.timerService.StartTimer()
+            this.currentSub = intervalObservable.subscribe(
+                (secondsPassed)=>{
+                    console.log(secondsPassed);
+                    this.updateTimeFields()
+                }
+            )
+        }
     }
     updateTimeFields()
     {
@@ -62,18 +70,36 @@ export class TimePlayerComponent implements OnDestroy,OnChanges{
         this.secondsLeft = updatedTime%60
     }
     onPauseButton()
-    { 
+    {
+        this.state = TimerState.PAUSED
         this.currentSub.unsubscribe()
         this.timerService.StopTimer()
     }
 
     onResetButton()
     {
+        this.state = TimerState.OFF
         if (this.currentSub!=undefined || this.currentSub!=null)
         {
             this.currentSub.unsubscribe()
         }
         this.timerService.ResetTimer()
         this.setTimeBasedOfCfg()
+    }
+    isTimerRunning()
+    {
+        if(this.state==TimerState.RUNNING)
+        {
+            return true;
+        }
+        return false;
+    }
+    isTimerPaused()
+    {
+        if(this.state==TimerState.PAUSED)
+        {
+            return true;
+        }
+        return false;
     }
 }
