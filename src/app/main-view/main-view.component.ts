86d7dc2db5982d17a6c7939d03d8a2c62458ec17
Robert Cordova <77909709+rcordova233TSLA@users.noreply.github.com>
2025-04-20 22:13:01 -0400

    add timer and task list to main view

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TaskListComponent } from '../task-list/task-list.component';
+import { TimePlayerComponent } from "../time-player/time-player.component";
 
 @Component({
   selector: 'app-main-view',
-  imports: [TaskListComponent],
+  imports: [TaskListComponent, TimePlayerComponent],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
