2ef90a7575ad451dae5b6fd82bc8a8af807d8ccd
Robert Cordova <77909709+rcordova233TSLA@users.noreply.github.com>
2025-04-21 22:08:51 -0400

    add new sprint-break view to main

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskListComponent } from '../task-list/task-list.component';
 import { TimePlayerComponent } from "../time-player/time-player.component";
+import { SprintBreakViewComponent } from "../sprint-break-view/sprint-break-view.component";
 
 @Component({
   selector: 'app-main-view',
-  imports: [TaskListComponent, TimePlayerComponent],
+  imports: [TaskListComponent, SprintBreakViewComponent],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
