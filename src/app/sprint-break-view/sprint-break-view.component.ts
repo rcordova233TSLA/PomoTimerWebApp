74708cdbe0ef2a83e5777c6b8bc3e682d4e44211
Robert Cordova <77909709+rcordova233TSLA@users.noreply.github.com>
2025-04-21 22:03:53 -0400

    add new override timer property to deal with config button presses

--- src/app/sprint-break-view/sprint-break-view.component.ts
+++ src/app/sprint-break-view/sprint-break-view.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { TimePlayerComponent } from '../time-player/time-player.component';
+import { Duration } from '../time-player/TimerConfiguration';
+const workConfig:Duration = {minutes:25,seconds:0}
+const shortBreakConfig:Duration = {minutes:5,seconds:0}
+const longBreakConfig:Duration = {minutes:15,seconds:0}
 
 @Component({
   selector: 'app-sprint-break-view',
@@ -8,5 +12,22 @@ import { TimePlayerComponent } from '../time-player/time-player.component';
   styleUrl: './sprint-break-view.component.scss'
 })
 export class SprintBreakViewComponent {
+    configuration:Duration = workConfig
+    overrideTimer: boolean =false;
+    onWork()
+    {
+        this.configuration = workConfig;
+        this.overrideTimer = true;
 
+    }
+    onShort()
+    {
+        this.configuration = shortBreakConfig;
+        this.overrideTimer = true;
+    }
+    onLong()
+    {
+        this.configuration = longBreakConfig;
+        this.overrideTimer = true;
+    }
 }
